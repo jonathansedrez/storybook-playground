cdf96a4248d114472af26d0aae467964d5d2292b
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-07-13 23:22:25 -0300

    feat: add interaction to registration form

--- src/stories/RegistrationForm.stories.tsx
+++ src/stories/RegistrationForm.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { RegistrationForm } from "../components/RegistrationForm";
-import { userEvent } from "storybook/internal/test";
 
 const meta = {
   component: RegistrationForm,
